de1814ef53215a09ceb99b0f54f5ea0a229c70fb
LuisCuaran <albert10luis@gmail.com>
2024-02-04 15:48:50 -0500

    v2.0 ghmp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './modulos/principal.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+import { UsuariosComponent } from './modulos/usuarios/usuarios.component';
+import { AdministracionComponent } from './modulos/administracion/administracion.component';
+import { RegistrosComponent } from './modulos/registros/registros.component';
+import { ProcedimientosComponent } from './modulos/procedimientos/procedimientos.component';
+import { ProveedorComponent } from './modulos/proveedor/proveedor.component';
+import { CategoriaComponent } from './modulos/categoria/categoria.component';
+import { HerramientasComponent } from './modulos/herramientas/herramientas.component';
+import { InventarioComponent } from './modulos/inventario/inventario.component';
+import { MantenimientoComponent } from './modulos/mantenimiento/mantenimiento.component';
+import { LoginComponent } from './modulos/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+
+  path: "",component: PrincipalComponent,
+  children: [
+    {path: "dashboard", component: DashboardComponent},
+    {path: "usuarios", component: UsuariosComponent},
+    {path: "administracion", component: AdministracionComponent},
+    {path: "registros", component: RegistrosComponent},
+    {path: "procedimientos", component: ProcedimientosComponent},
+    {path: "proveedor", component: ProveedorComponent},
+    {path: "categoria", component: CategoriaComponent},
+    {path: "herramientas", component: HerramientasComponent},
+    {path: "inventario", component: InventarioComponent},
+    {path: "mantenimiento", component: MantenimientoComponent},
+    {path: "", redirectTo: "/dashboard", pathMatch: "full"}
+  ]
+},
+{ path: "Login", component: LoginComponent},
+
+];
+
+//const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './etructura/header/header.component';
+import { NavComponent } from './etructura/nav/nav.component';
+import { FooterComponent } from './etructura/footer/footer.component';
+import { PrincipalComponent } from './modulos/principal.component';
+import { AdministracionComponent } from './modulos/administracion/administracion.component';
+import { UsuariosComponent } from './modulos/usuarios/usuarios.component';
+import { RegistrosComponent } from './modulos/registros/registros.component';
+import { ProcedimientosComponent } from './modulos/procedimientos/procedimientos.component';
+import { ProveedorComponent } from './modulos/proveedor/proveedor.component';
+import { CategoriaComponent } from './modulos/categoria/categoria.component';
+import { HerramientasComponent } from './modulos/herramientas/herramientas.component';
+import { InventarioComponent } from './modulos/inventario/inventario.component';
+import { MantenimientoComponent } from './modulos/mantenimiento/mantenimiento.component';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+import { LoginComponent } from './modulos/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    PrincipalComponent,
+    AdministracionComponent,
+    UsuariosComponent,
+    RegistrosComponent,
+    ProcedimientosComponent,
+    ProveedorComponent,
+    CategoriaComponent,
+    HerramientasComponent,
+    InventarioComponent,
+    MantenimientoComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
